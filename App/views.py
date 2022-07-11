458c4f3891c04bb6e224ceaaea8b2e6c647b59bc
zaheerali4405 <zaheerali4405@gmail.com>
2022-07-11 19:10:03 +0500

    Creating Views and image folder directory

--- App/views.py
+++ App/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    return render(request, 'index.html')
+
+def login(request):
+    return render(request, 'login.html')
+
+def signUp(request):
+    return render(request, 'signUp.html')
